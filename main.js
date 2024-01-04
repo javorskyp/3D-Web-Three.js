8541449fad290559f9bfc494510520a3fbb289f4
Pawel Jaworski Software <jpsoftware@MacBook-Pro-Pawel.local>
2024-01-04 19:37:41 +0100

    Render & light

--- main.js
+++ main.js
@@ -2,4 +2,27 @@ import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 
-const geometry = new THREE.SphereGeometry(3, 64, 64)
+//Create sphere
+const geometry = new THREE.SphereGeometry(3, 64, 64);
+const material = new THREE.MeshStandardMaterial({
+  color: "#00ff83",
+})
+
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+//Light
+const light = new THREE.PointLight(0xffffff, 1, 100)
+light.position.set(0, 10, 10)
+scene.add(light)
+
+//Camera
+const camera = new THREE.PerspectiveCamera(45, 800 / 600)
+camera.position.z = 20;
+scene.add(camera)
+
+//Render
+const canvas = document.querySelector('.webgl');
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(800, 600);
+renderer.render(scene, camera);
