ba5e6c420432ec93db4bc8d40a649db4b2123187
Pawel Jaworski Software <jpsoftware@mbp-pawel.home>
2024-01-09 23:12:07 +0100

    mouseAnimation

--- main.js
+++ main.js
@@ -66,5 +66,29 @@ loop()
 // Timeline GSAP
 
 const timeLine1 = gsap.timeline({defaults: {duration: 1}}) 
-timeLine1.from.To(mesh.scale, {z: 0, x: 0, y: 0}, {z:1, x:1, y:1})
+timeLine1.fromTo(mesh.scale, {z: 0, x: 0, y: 0}, {z:1, x:1, y:1})
+timeLine1.fromTo("nav", {y: "-100%" }, {y: "0%"})
+timeLine1.fromTo(".title", { opacity: 0 }, { opacity: 1})
 
+//Mouse Animation Color
+let mouseDown = false
+let rgb = []
+window.addEventListener("mousedown", () => (mouseDown = true))
+window.addEventListener("mouseup", () => (mouseDown = false))
+
+window.addEventListener("mousemove", (e) => {
+  if(mouseDown) {
+    rgb = [
+      Math.round((e.pageX / sizes.width) * 255),
+      Math.round((e.pageY / sizes.height) * 255),
+      150,
+    ]
+    //Animate this
+    let newColor = new THREE.Color(`rgb(${rgb.join(",")})`)
+    gsap.to(mesh.material.color, {
+      r: newColor.r,
+      g: newColor.g,
+      b: newColor.b
+    })
+  }
+})
